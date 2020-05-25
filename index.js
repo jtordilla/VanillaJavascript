fdcff9ab6a5e96ee719b8dddc4fea0ae7967fb7b
Jonathon T <jtordilla03@gmail.com>
2020-05-25 16:24:17 -0700

    new schedule app and addition of prompts and confirmation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+//DOM: innerHTML, style, animations and intervals, event listeners, and nodes
+//BOM: 
+
 function changeAppear(){
     var message = document.getElementById("message"); 
     if(message.innerHTML == "Hello World!"){
-        message.innerHTML = "Vanilla JS is superior!"; 
+        if(prompt("Are you sure you want to continue?") === "Yes"){
+            message.innerHTML = "Vanilla JS is superior!"; 
+        }else{
+            message.innerHTML = "Hello World!"; 
+        }
     }else{
         message.innerHTML = "Hello World!"; 
     }
@@ -36,16 +43,20 @@ function hideAll(){
 }
 
 function animateSquare(){
-    var pos = 100; 
-    var timer = setInterval(frame, 5); 
-    var square = document.getElementById("object"); 
-    function frame(){
-        if(pos == 350){
-            clearInterval(timer); 
-        }else{
-            pos++; 
-            square.style.bottom = pos + "px"; 
-            square.style.left = pos + "px"; 
+    if(confirm("Are you sure you want to continue?")){
+        var pos = 100; 
+        var timer = setInterval(frame, 5); 
+        var square = document.getElementById("object"); 
+        function frame(){
+            if(pos == 350){
+                clearInterval(timer); 
+            }else{
+                pos++; 
+                square.style.bottom = pos + "px"; 
+                square.style.left = pos + "px"; 
+            }
         }
+    }else{
+        console.log("animation cancelled"); 
     }
 }
